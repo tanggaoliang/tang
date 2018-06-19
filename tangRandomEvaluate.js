a5b831d6d6c1629418011e51cd3045ee1c6186b7
tanggaoliang <1440535574@qq.com>
2018-06-19 20:23:53 +0800

    Delete tangRandomEvaluate.js

--- tangRandomEvaluate.js
+++
@@ -1,7 +0,0 @@
-<a href="
-javascript:(function () {
-    for (var i = 1; i <= 10; i++) {
-        var j = Math.floor(Math.random() * 4);
-        document.getElementById("option_" + i + "_" + j).checked = true;
-    }
-})()">随机评教</a>
